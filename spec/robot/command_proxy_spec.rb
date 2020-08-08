97cae2d74ae7dc1ce60974f704ef0a3d0a1a9bb0
Ahmad Al-kheat <ahmad@himama.com>
2020-08-08 13:55:49 +1000

    proxy spec

--- spec/robot/command_proxy_spec.rb
+++ spec/robot/command_proxy_spec.rb
@@ -9,7 +9,7 @@ describe Robot::CommandProxy do
   let(:left) { 'LEFT' }
   let(:place) { 'PLACE 0,0,NORTH' }
 
-  context 'when within table' do
+  context 'when within the table limits' do
     let(:point) { Robot::Point.new(x: 0, y: 0) }
 
     it 'applies the command and returns the new position' do
@@ -18,12 +18,19 @@ describe Robot::CommandProxy do
     end
   end
 
-  context 'outside of table' do
+  context 'when outside of the table limits' do
     let(:point) { Robot::Point.new(x: 0, y: 4)}
 
     it 'ignores the command and returns the same position' do
       expect(described_class.new(command_string: move, position: position).()).to(
         eq(position))
+    end
+  end
+
+  context 'when not placed yet' do
+    it 'ignores the command' do
+      expect(described_class.new(command_string: move, position: nil).()).to(
+        eq(nil))
     end
   end
 end
