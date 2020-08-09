2f0f23745a4dc713236ff2db7c6f2abd2e779dc5
Ahmad Al-kheat <ahmad@himama.com>
2020-08-09 18:25:56 +1000

    instructions

--- lib/robot/game.rb
+++ lib/robot/game.rb
@@ -1,6 +1,7 @@
 module Robot
   class Game
     attr_reader :input
+
     def initialize(input: $stdin)
       @input = input
     end
@@ -8,7 +9,6 @@ module Robot
     def simulate
       while command = input.gets
         command = command.chomp
-
         @position = Robot::CommandProxy.new(command_string: command, position: @position).call
       end
     end
@@ -16,6 +16,21 @@ module Robot
     def self.call(input: $stdin)
       game = new(input: input)
       game.simulate
+      instruction
+    end
+
+    def self.instruction
+      puts <<~HEREDOC
+        Welcome to the Robot game
+        Please enter one of the following commands:
+        PLACE 0,0,NORTH (PLACE X,Y,DIRECTION)
+        MOVE
+        LEFT
+        RIGHT
+
+        invalid commands are ignored.
+        You must start with a place command.
+      HEREDOC
     end
   end
 end
